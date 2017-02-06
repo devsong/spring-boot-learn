fe68238100ef67e870a29ca8cddfa2743a5a461f
guanzhisong <guanzhisong@gomeplus.com>
2017-02-06 14:27:19 +0800

    add jpa example

--- src/main/java/com/gzs/learn/boot/jpa/BootApplication.java
+++ src/main/java/com/gzs/learn/boot/jpa/BootApplication.java
@@ -1,10 +1,14 @@
 package com.gzs.learn.boot.jpa;
 
+<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
+=======
+>>>>>>> add jpa example
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+<<<<<<< HEAD
 import com.gzs.learn.boot.jpa.repository.TestHbRepository;
 
 @SpringBootApplication
@@ -13,6 +17,11 @@ public class BootApplication {
 	@Autowired
 	private TestHbRepository repository;
 
+=======
+@SpringBootApplication
+@EnableAutoConfiguration
+public class BootApplication {
+>>>>>>> add jpa example
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 
