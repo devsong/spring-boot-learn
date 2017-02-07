91b40beb57525b92eb71783d817f264b52696e97
guanzhisong <guanzhisong@gomeplus.com>
2017-02-07 14:10:00 +0800

    add serializable

---
+++ src/main/java/com/gzs/learn/boot/serializable/TestSerializable.java
@@ -0,0 +1,46 @@
+package com.gzs.learn.boot.serializable;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Test;
+
+import lombok.Data;
+
+public class TestSerializable {
+	String filename = "d:/test.obj";
+
+	@Test
+	public void testWriteObject() throws FileNotFoundException, IOException {
+		Serializable serializable = new Serializable();
+		serializable.setName("test");
+		// serializable.setTest("1");
+		ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(filename));
+
+		os.writeObject(serializable);
+		os.close();
+	}
+
+	@Test
+	public void testReadObject() throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+		Serializable s = (Serializable) in.readObject();
+		System.out.println(s.getName());
+		// System.out.println(s.getTest());
+		in.close();
+	}
+
+}
+
+@Data
+class Serializable implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	// private String test;
+}
